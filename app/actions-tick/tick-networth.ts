0f9216346e51646112632b9322e17b8e8e32d863
Ričardas Jaksebaga <rij@open.ch>
2023-12-01 19:30:21 +0100

    fix nw change

--- app/actions-tick/tick-networth.ts
+++ app/actions-tick/tick-networth.ts
@@ -3,5 +3,5 @@ import { kdUtil } from '~/kingdom/kd.util';
 
 export const tickNetworth = (kdid: number, app: AppState) => {
 	const { status, military, buildings } = kdUtil.getFullKingdom(kdid, app);
-	return kdUtil.getNetworth(status, buildings, military);
+	return Math.floor(kdUtil.getNetworth(status, buildings, military));
 };
